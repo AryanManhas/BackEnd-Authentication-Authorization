69d2d06c22eca1faed0134fed3eb82606889e677
Aryan Manhas <2022A1R033@mietjammu.in>
2025-09-08 23:44:48 +0530

    authentication

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 const bcrypt = require('bcrypt');
 
 app.get("/" , (req , res)=>{
-    res.send("done")
-})
 
-app.get("/read" , (req , res)=>{
-     res.send("read page!!")
-})
+    bcrypt.compare("polololoo", "$2b$10$AMHdStrG4ZaW3YKsj1ZXkuAkk9LIN5jjxQ2fZxnOXvPGA0.NxiQBW", function(err, result) {
+        console.log(result);
+    });
+    });
+
 
 app.listen(3000);
